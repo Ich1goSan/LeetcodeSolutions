2d8aae12dbf0da3744b0ff969783af5d901439d6
hazardinho <alpuspaev.daniyar@gmail.com>
2019-12-25 20:57:43 +0600

    problem #657 - Robot Return to Origin

---
+++ judgeCircle.py
@@ -0,0 +1,12 @@
+def judgeCircle(self, moves: str) -> bool:
+        x, y = 0, 0
+        for i in moves:
+            if i == "U":
+                x+=1
+            elif i == "D":
+                x-=1
+            elif i == "R":
+                y+=1
+            elif i == "L":
+                y-=1
+        return x == 0 and y == 0
