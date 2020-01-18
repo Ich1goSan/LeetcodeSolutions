427b03b505e08a8d48f3e25807c1b81eba1d216c
Ich1goSan <daniyar.alpyspayev@yandex.com>
2020-01-18 16:00:05 +0600

    problem #9 - Palindrome Number (faster updated solution)

--- isPalindrome.py
+++ isPalindrome.py
@@ -1,3 +1,6 @@
 def isPalindrome(self, x: int) -> bool:
     x = str(x)
-    return x == x[::-1]
+    for i in range(len(x)):
+        if x[i] != x[len(x)-1-i]:
+            return False
+    return True
