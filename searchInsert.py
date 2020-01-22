9ec5bd08409fcdf1b1f8da0ec51e5b8a53f657ee
Ichigo <00047844@nb>
2020-01-22 14:10:55 +0600

    problem #35 - Search Insert Position

---
+++ searchInsert.py
@@ -0,0 +1,12 @@
+def searchInsert(self, nums: List[int], target: int) -> int:
+    l = 0
+    r = len(nums) - 1
+    while l <= r:
+        mid = l + (r - l) // 2
+        if nums[mid] == target:
+            return mid
+        if nums[mid] < target:
+            l = mid + 1
+        else:
+            r = mid - 1
+    return l
