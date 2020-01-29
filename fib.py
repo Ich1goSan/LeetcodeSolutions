b6def3e987aae1d86dbe3eb9418657bbe0d23b03
Ich1goSan <daniyar.alpyspayev@yandex.com>
2020-01-29 08:44:38 +0600

    problem #503 - Fibonacci Number (dp solution)

--- fib.py
+++ fib.py
@@ -1,4 +1,12 @@
-def fib(self, N: int) -> int:
-    if N <=1:
-        return N
-    return self.fib(N-1)+self.fib(N-2)
+def fib(self, N):
+    if N == 0:
+        return 0
+    if N == 1:
+        return 1
+
+    dp = [None] * (N+1)
+    dp[0] = 0
+    dp[1] = 1
+    for i in range(2, N+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    return dp[N]
