42678168b5fe6d610c753382d5c5a1f7d436e505
Ich1goSan <daniyar.alpyspayev@yandex.com>
2020-01-29 08:32:26 +0600

    problem #70 - Climbing Stairs

---
+++ climbingStairs.py
@@ -0,0 +1,14 @@
+def climbStairs(self, n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    dp = [None] * (n+1)
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    return dp[n]
