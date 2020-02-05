0b13091ebdb00d5144379a11bc6ccb94d3e5ec6a
Ich1goSan <daniyar.alpyspayev@yandex.com>
2020-02-05 08:06:40 +0600

    problem #2 - Add Two Numbers (a little bit of optimization)

--- addTwoNumbers.py
+++ addTwoNumbers.py
@@ -1,4 +1,4 @@
-def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+def addTwoNumbers(self, l1, l2):
     num1 = ''
     num2 = ''
     while l1:
@@ -7,10 +7,10 @@ def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
     while l2:
         num2 = str(l2.val) + num2
         l2 = l2.next
-    outStr = str(int(num1) + int(num2))[::-1]
-    node = ListNode(int(outStr[0]))
+    outStr = str(int(num1) + int(num2))
+    node = ListNode(int(outStr[-1]))
     current = node
-    for i in range(1, len(outStr)):
+    for i in range(len(outStr)-2, -1, -1):
         newNode = ListNode(int(outStr[i]))
         current.next = newNode
         current = current.next
